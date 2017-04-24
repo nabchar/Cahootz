3e23b4c8cd33c708385625e2c4dab485a71459d0
Nick Char <nick.char.nick@gmail.com>
2017-04-24 18:01:36 -0400

    Add seeds for messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,9 @@ Message.destroy_all
 admin = User.create!(username: "admin", password: "hardpassword")
 user1 = User.create!(username: "guest", password: "password")
 user2 = User.create!(username: "nabchar", password: "starwars")
-
-users = [user1, user2]
+user3 = User.create!(username: "darthV", password: "vaderbaby")
+user4 = User.create!(username: "lukeS", password: "skywalker")
+users = [user1, user2, user3, user4]
 
 channel1 = Channel.create!(name: "general", purpose: "General Discussion", description: "", user_id: admin.id)
 channel2 = Channel.create!(name: "random", purpose: "Random Discussion", description: "", user_id: admin.id)
@@ -34,3 +35,6 @@ users.each do |user|
   user.previous_channel_id = generalChannel;
   user.save!
 end
+
+m1 = Message.create!(user_id: user3.id, channel_id: channel1.id, content: "Luke, I am your father...")
+m2 = Message.create!(user_id: user4.id, channel_id: channel1.id, content: "Nooooooooo!!!!!")
